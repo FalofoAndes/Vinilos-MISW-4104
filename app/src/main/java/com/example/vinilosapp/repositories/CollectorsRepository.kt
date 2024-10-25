869767e7f737fafe2a69feeda4c29c7854c326ce
daaruiz_cib <daaruiz@bancolombia.com.co>
2024-10-25 15:23:03 -0500

    Se agregó una estructura base para el proyecto y la lógica de coleccionista.

---
+++ app/src/main/java/com/example/vinilosapp/repositories/CollectorsRepository.kt
@@ -0,0 +1,13 @@
+package com.example.vinilosapp.repositories
+
+import android.app.Application
+import com.example.vinilosapp.models.Collector
+import com.example.vinilosapp.network.NetworkServiceAdapter
+
+class CollectorsRepository (val application: Application){
+    suspend fun refreshData(): List<Collector> {
+        //Determinar la fuente de datos que se va a utilizar. Si es necesario consultar la red, ejecutar el siguiente código
+        return NetworkServiceAdapter.getInstance(application).getCollectors()
+    }
+
+}
