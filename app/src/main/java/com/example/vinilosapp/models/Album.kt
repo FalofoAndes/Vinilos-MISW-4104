34c32bb3cb43b85009ff5dd4e74b50a970b78672
daaruiz_cib <daaruiz@bancolombia.com.co>
2024-10-25 19:36:57 -0500

    Update estructura proyecto

--- app/src/main/java/com/example/vinilosapp/models/Album.kt
+++
@@ -1,8 +0,0 @@
-package com.example.vinilosapp.models
-
-data class Album (
-    var collectorId: Int? = null,
-    var name:String? = null,
-    var telephone:String? = null,
-    var email:String? = null
-)
