831a6cc618d2841a649b31a720859084d65744d5
FalofoAndes <f.lozanof@uniandes.edu.co>
2024-10-30 20:43:43 -0500

    feat: se agrega listado de track en detalle de album

--- app/src/main/java/com/example/vinilos_app/ui/AlbumDetailFragment.kt
+++ app/src/main/java/com/example/vinilos_app/ui/AlbumDetailFragment.kt
@@ -15,8 +15,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.vinilos_app.models.Album
 import com.example.vinilos_app.repository.AlbumRepository
 import com.example.vinilos_app.ui.adapter.AlbumDetailAdapter
+import com.example.vinilos_app.ui.adapter.TrackAdapter
 import com.example.vinilos_app.viewmodels.AlbumDetailViewModel
 import com.example.vinyls_jetpack_application.R
+import com.example.vinyls_jetpack_application.databinding.AlbumDetailItemBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -25,10 +27,11 @@ import com.example.vinyls_jetpack_application.R
  */
 class AlbumDetailFragment : Fragment() {
 
-    private var album: Album? = null // Este es tu modelo `Album`, debes obtener o crear la instancia de alguna forma
     private lateinit var albumDetailViewModel: AlbumDetailViewModel
     private lateinit var adapter: AlbumDetailAdapter
     private var albumId: Int? = null
+    private lateinit var binding: AlbumDetailItemBinding;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +48,7 @@ class AlbumDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.album_detail_fragment, container, false)
+        binding = AlbumDetailItemBinding.inflate(inflater, container, false)
 
         // Recuperar el ID del álbum pasado como argumento
         albumId = arguments?.let { AlbumDetailFragmentArgs.fromBundle(it).idAlbum }
@@ -54,7 +58,6 @@ class AlbumDetailFragment : Fragment() {
             if (album != null) {
                 // Inicializar el adaptador
                 adapter = AlbumDetailAdapter(album)
-                // Suponiendo que tienes un RecyclerView o algún contenedor para mostrar el álbum
                 val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view_album_detail)
                 recyclerView.adapter = adapter
                 recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -63,7 +66,6 @@ class AlbumDetailFragment : Fragment() {
 
         // Observar errores
         albumDetailViewModel.error.observe(viewLifecycleOwner, Observer { errorMessage ->
-            // Aquí puedes mostrar un mensaje de error al usuario, por ejemplo con un Toast o un Snackbar
             Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
         })
 
@@ -72,11 +74,27 @@ class AlbumDetailFragment : Fragment() {
 
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Inicializa el RecyclerView y su adaptador con una lista vacía inicialmente
+        val adapter = TrackAdapter(emptyList())
+        binding.recyclerViewTracks.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewTracks.adapter = adapter
+
+        // Observa los detalles del álbum en el ViewModel
+        albumDetailViewModel.albumDetail.observe(viewLifecycleOwner) { album ->
+            album?.let {
+                binding.album = it // Asigna el álbum al binding
+                adapter.updateTracks(it.tracks)
+                adapter.notifyDataSetChanged() // Notifica al adaptador que los datos han cambiado
+            }
+        }
+
+    }
 }
 
-
-
-// Clase de fábrica para crear AlbumCatalogViewModel con argumentos
 class AlbumDetailViewModelFactory(private val repository: AlbumRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(AlbumDetailViewModel::class.java)) {
