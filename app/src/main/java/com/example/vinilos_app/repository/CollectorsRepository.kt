a1b2e433e657602a314801020bcd58e9c80a3758
daaruiz_cib <daaruiz@bancolombia.com.co>
2024-11-09 12:58:59 -0500

    Add Collectors Repository

---
+++ app/src/main/java/com/example/vinilos_app/repository/CollectorsRepository.kt
@@ -0,0 +1,16 @@
+package com.example.vinilos_app.repository
+
+import android.content.Context
+import com.android.volley.VolleyError
+import com.example.vinilos_app.models.Collector
+import com.example.vinilos_app.network.NetworkServiceAdapter
+
+class CollectorsRepository(private val context: Context) {
+
+    private val networkService = NetworkServiceAdapter.getInstance(context)
+
+    // Función para obtener la lista de Collectors
+    suspend fun getCollectorsCatalog(): List<Collector> {
+        return networkService.getCollectors()
+    }
+}
